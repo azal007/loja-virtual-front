b44bdfd1b9255e45a0c1708a48716df3b13ee0e0
Gabriel Nathan dos Santos Pires <devgabrielnathan@gmail.com>
2025-10-27 19:53:40 -0300

    atualizando rotas.

--- src/app/pages/usuarios/usuarios-routing.module.ts
+++ src/app/pages/usuarios/usuarios-routing.module.ts
@@ -1,30 +1,18 @@
 import { RouterModule, Routes }       from '@angular/router';
 import { NgModule }                   from '@angular/core';
-
-import { BuscarUsuariosComponent }    from './buscarPorId/buscar-usuarios-component';
 import { ListarUsuariosComponent }    from './listar/listar-usuarios-component';
 import { IncluirUsuariosComponent }   from './incluir/incluir-usuarios-component';
-import { AtualizarUsuariosComponent } from './atualizar/atualizar-usuarios-component';
 
 
 const routes: Routes = [{
   path: '',
-  children: [
-    {
-      path: 'buscar-por-id',
-      component: BuscarUsuariosComponent
-    },
-    {
-      path: 'listar',
-      component: ListarUsuariosComponent
-    },
-    {
+  children: [{
+    path: 'listar',
+    component: ListarUsuariosComponent
+  },
+  {
       path: 'incluir',
       component: IncluirUsuariosComponent
-    },
-    {
-      path: 'atualizar',
-      component: AtualizarUsuariosComponent
     },
   ]
 }];
@@ -36,8 +24,6 @@ const routes: Routes = [{
 export class UsuariosRoutingModule{ }
 
 export const routedComponents = [
-  BuscarUsuariosComponent,
   ListarUsuariosComponent,
   IncluirUsuariosComponent,
-  AtualizarUsuariosComponent,
 ]
